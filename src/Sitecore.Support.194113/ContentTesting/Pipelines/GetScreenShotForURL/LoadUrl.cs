93a8978282903e49db6046840bf0cc9cd53fb26c
DK\ili <ili@sitecore.net>
2017-11-17 14:53:38 +0200

    Original

---
+++ src/Sitecore.Support.194113/ContentTesting/Pipelines/GetScreenShotForURL/LoadUrl.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Text;
+using Sitecore.Web;
+using System;
+
+namespace Sitecore.ContentTesting.Pipelines.GetScreenShotForURL
+{
+  [Sitecore.UsedImplicitly]
+  public class LoadUrl : GenerateScreenShotProcessor
+  {
+    public override void Process(GetScreenShotForURLArgs args)
+    {
+      Sitecore.Diagnostics.Assert.IsNotNull(args, "args");
+      Sitecore.Text.UrlString urlString = new Sitecore.Text.UrlString(args.UrlParameters)
+      {
+        Path = "/"
+      };
+      args.Url = Sitecore.Web.WebUtil.GetServerUrl() + urlString.GetUrl();
+    }
+  }
+}
