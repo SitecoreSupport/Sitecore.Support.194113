ea10f33c7e9db19f369b3d09316b695287e324ce
DK\ili <ili@sitecore.net>
2017-11-20 12:51:46 +0200

    Release

--- src/Sitecore.Support.194113/ContentTesting/Pipelines/GetScreenShotForURL/LoadUrl.cs
+++ src/Sitecore.Support.194113/ContentTesting/Pipelines/GetScreenShotForURL/LoadUrl.cs
@@ -2,8 +2,13 @@
 using Sitecore.Text;
 using Sitecore.Web;
 using System;
+using Sitecore.Configuration;
+using Sitecore.Data.Items;
+using Sitecore.Data;
+using Sitecore.Links;
+using Sitecore.ContentTesting.Pipelines.GetScreenShotForURL;
 
-namespace Sitecore.ContentTesting.Pipelines.GetScreenShotForURL
+namespace Sitecore.Support.ContentTesting.Pipelines.GetScreenShotForURL
 {
   [Sitecore.UsedImplicitly]
   public class LoadUrl : GenerateScreenShotProcessor
@@ -15,7 +20,32 @@ namespace Sitecore.ContentTesting.Pipelines.GetScreenShotForURL
       {
         Path = "/"
       };
-      args.Url = Sitecore.Web.WebUtil.GetServerUrl() + urlString.GetUrl();
+
+      #region Added Code
+      if (Sitecore.Configuration.Settings.GetBoolSetting("Rendering.SiteResolving", true))
+      {
+        Item item = Database.GetDatabase("master").GetItem(args.ItemId);
+        var siteInfoList = Sitecore.Configuration.Factory.GetSiteInfoList();
+        foreach (SiteInfo siteInf in siteInfoList)
+        {
+          if (!String.IsNullOrEmpty(siteInf.RootPath) && !String.IsNullOrEmpty(siteInf.StartItem) && item.Paths.FullPath.StartsWith(siteInf.RootPath + siteInf.StartItem))
+          {
+            Sitecore.Context.Site = Sitecore.Sites.SiteContext.GetSite(siteInf.Name);
+
+            args.Url = String.Format("{0}://{1}", siteInf.Scheme, siteInf.TargetHostName) + urlString.GetUrl();
+          }
+        }
+
+      }
+
+      else
+
+      {
+
+        #endregion
+
+        args.Url = Sitecore.Web.WebUtil.GetServerUrl() + urlString.GetUrl();
+      }
     }
   }
 }
